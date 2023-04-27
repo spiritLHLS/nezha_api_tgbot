1dcc78497d9cd2070e11b07eea3e42c59fd1b77f
SuperYuIuo <39399442+SuperYuIuo@users.noreply.github.com>
2023-04-27 21:11:12 +0800

    Update quick.sh

--- quick.sh
+++ quick.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+utf8_locale=$(locale -a 2>/dev/null | grep -i -m 1 -E "UTF-8|utf8")
+if [[ -z "$utf8_locale" ]]; then
+  echo "No UTF-8 locale found"
+else
+  export LC_ALL="$utf8_locale"
+  export LANG="$utf8_locale"
+  export LANGUAGE="$utf8_locale"
+  echo "Locale set to $utf8_locale"
+fi
+
 # 必须以root运行脚本
 check_root(){
   [[ $(id -u) != 0 ]] && red " The script must be run as root, you can enter sudo -i and then download and run again." && exit 1
